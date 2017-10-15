33551472534b8ca8930277cf4fc42aa2924cab0a
James Kozianski <james@helixta.com.au>
2017-10-15 23:07:06 +1100

    Update autogenerated code.

--- api/api.pb.gw.go
+++ api/api.pb.gw.go
@@ -143,6 +143,19 @@ func request_ApiService_AddTalk_0(ctx context.Context, marshaler runtime.Marshal
 
 }
 
+func request_ApiService_CompleteTalk_0(ctx context.Context, marshaler runtime.Marshaler, client ApiServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CompleteTalkRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.CompleteTalk(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 // RegisterApiServiceHandlerFromEndpoint is same as RegisterApiServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterApiServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -376,6 +389,35 @@ func RegisterApiServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn 
 
 	})
 
+	mux.Handle("POST", pattern_ApiService_CompleteTalk_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_ApiService_CompleteTalk_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_ApiService_CompleteTalk_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -393,6 +435,8 @@ var (
 	pattern_ApiService_Reorder_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "reorder"}, ""))
 
 	pattern_ApiService_AddTalk_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "talk"}, ""))
+
+	pattern_ApiService_CompleteTalk_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "talk", "complete"}, ""))
 )
 
 var (
@@ -409,4 +453,6 @@ var (
 	forward_ApiService_Reorder_0 = runtime.ForwardResponseMessage
 
 	forward_ApiService_AddTalk_0 = runtime.ForwardResponseMessage
+
+	forward_ApiService_CompleteTalk_0 = runtime.ForwardResponseMessage
 )
